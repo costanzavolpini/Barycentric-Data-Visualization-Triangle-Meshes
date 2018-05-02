4dc65fc29f1c8370478e9255a5e9d88a394076c3
Costanza Volpini <costanza.volpini@gmail.com>
2018-05-02 23:15:25 +0200

    Clean code

--- source/main.cpp
+++ source/main.cpp
@@ -323,28 +323,6 @@ int main(int argc, char * argv[]) {  //arguments: nameFile type(example: gc is g
 		return false;
 
 
-    // The fullscreen quad's FBO
-	static const GLfloat g_quad_vertex_buffer_data[] = {
-		-1.0f, -1.0f, 0.0f,
-		 1.0f, -1.0f, 0.0f,
-		-1.0f,  1.0f, 0.0f,
-		-1.0f,  1.0f, 0.0f,
-		 1.0f, -1.0f, 0.0f,
-		 1.0f,  1.0f, 0.0f,
-	};
-
-	GLuint quad_vertexbuffer;
-	glGenBuffers(1, &quad_vertexbuffer);
-	glBindBuffer(GL_ARRAY_BUFFER, quad_vertexbuffer);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(g_quad_vertex_buffer_data), g_quad_vertex_buffer_data, GL_STATIC_DRAW);
-
-	// Create and compile our GLSL program from the shaders
-    Shader quadShader("bufferTexture.vs", "bufferTexture.fs");
-    quadShader.use();
-	GLuint texID = glGetUniformLocation(quadShader.shaderProgram, "renderedTexture");
-	GLuint timeID = glGetUniformLocation(quadShader.shaderProgram, "time");
-
-
     // --------------- IMGUI ---------------------
     // Setup Dear ImGui binding
     IMGUI_CHECKVERSION();
@@ -446,40 +424,6 @@ int main(int argc, char * argv[]) {  //arguments: nameFile type(example: gc is g
         // Render to the screen
 		glBindFramebuffer(GL_FRAMEBUFFER, 0);
 
-        // Render on the whole framebuffer, complete from the lower left corner to the upper right
-		glViewport(0, 0, windowWidth, windowHeight);
-
-		// Clear the screen
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-		// Use our shader
-        quadShader.use();
-
-        // Bind our texture in Texture Unit 0
-		glActiveTexture(GL_TEXTURE0);
-		glBindTexture(GL_TEXTURE_2D, rendered_texture);
-
-		// // Set our "renderedTexture" sampler to use Texture Unit 0
-		// glUniform1i(texID, 0);
-
-		// glUniform1f(timeID, (float)(glfwGetTime() * 10.0f));
-
-        // // 1rst attribute buffer : vertices
-		// glEnableVertexAttribArray(0);
-		// glBindBuffer(GL_ARRAY_BUFFER, quad_vertexbuffer);
-		// glVertexAttribPointer(
-		// 	0,                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
-		// 	3,                  // size
-		// 	GL_FLOAT,           // type
-		// 	GL_FALSE,           // normalized?
-		// 	0,                  // stride
-		// 	(void*)0            // array buffer offset
-		// );
-
-		// // Draw the triangles !
-		// glDrawArrays(GL_TRIANGLES, 0, 6); // 2*3 indices starting at 0 -> 2 triangles
-
-		// glDisableVertexAttribArray(0);
 
         // ---------- END RENDER AS TEXTURE (FBO) --------------------
 
@@ -498,7 +442,6 @@ int main(int argc, char * argv[]) {  //arguments: nameFile type(example: gc is g
     }
 
     ourShader.deactivate();
-    quadShader.deactivate();
 
     if (IS_IN_DEBUG){
         normalShader.deactivate();
@@ -509,7 +452,6 @@ int main(int argc, char * argv[]) {  //arguments: nameFile type(example: gc is g
     glDeleteFramebuffers(1, &frame_buffer);
 	glDeleteTextures(1, &rendered_texture);
 	glDeleteRenderbuffers(1, &depth_render_buffer);
-	glDeleteBuffers(1, &quad_vertexbuffer);
 
     ImGui_ImplGlfwGL3_Shutdown();
     ImGui::DestroyContext();
@@ -686,6 +628,7 @@ void rotation_settings(){
 }
 
 
+// function to set zoom into imgui window
 void zoom_settings(){
     ImGui::Text("Zoom-in/out:");
     ImGui::SliderFloat("zoom", &Zoom, 100, 1); // Zoom
